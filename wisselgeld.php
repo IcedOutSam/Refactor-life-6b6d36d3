a6c66b6c6c5987beffed47eb7499ece0a473ecd3
Sam Ahmed <2102926@talnet.nl>
2020-03-26 16:30:25 +0100

    Uitwerking van de exercise

--- wisselgeld.php
+++ wisselgeld.php
@@ -1,30 +1,30 @@
 
  <?php
- $Doekoe = array('50', '20', '10', '5', '2', '1');
- $aantal = $argv[1];
- $Bruh = intval($aantal);
- $restanten = $aantal;
- $aantal = round($aantal += 0.02, 2, PHP_ROUND_HALF_UP);
- $compleetCentjes = ($aantal - $Bruh) * 100;
- $restantenCentjes = $compleetCentjes;
- foreach ($Doekoe  as $value) {
-   if ($aantal > 0) {
-     $totaal = floor($restanten / $value);
+ $euroArray = array('50', '20', '10', '5', '2', '1');
+ $inputTotaal = $argv[1];
+ $euroTotaal = intval($inputTotaal);
+ $restanten = $inputTotaal;
+ $inputTotaal = round($inputTotaal += 0.02, 2, PHP_ROUND_HALF_UP);
+ $inputCent = ($inputTotaal - $euroTotaal) * 100;
+ $restantenCentjes = $inputCent;
+ foreach ($euroArray  as $waarden) {
+   if ($inputTotaal > 0) {
+     $totaal = floor($restanten / $waarden);
      if ($totaal > 0) {
-         echo "U krijgt $totaal x €$value " . PHP_EOL;
-         $aantal = $restanten;
-         $restanten = floor($restanten % $value);
+         echo "U krijgt $totaal x €$waarden " . PHP_EOL;
+         $inputTotaal = $restanten;
+         $restanten = floor($restanten % $waarden);
      }
    }
  }
  $centjes = array('50', '20', '10', '5');
- foreach ($centjes as $value2) {
-   if ($compleetCentjes > 0) {
-     $totaalCentjes = floor($restantenCentjes / $value2);
+ foreach ($centjes as $waarden2) {
+   if ($inputCent > 0) {
+     $totaalCentjes = floor($restantenCentjes / $waarden2);
      if ($totaalCentjes > 0) {
-       echo "U krijgt $totaalCentjes x $value2 cent" . PHP_EOL;
-       $compleetCentjes = $restantenCentjes;
-       $restantenCentjes = ($restantenCentjes % $value2);
+       echo "U krijgt $totaalCentjes x $waarden2 cent" . PHP_EOL;
+       $inputCent = $restantenCentjes;
+       $restantenCentjes = ($restantenCentjes % $waarden2);
      }
    }
  }
